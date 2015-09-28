b8b1440a556d126e77a61efa9ccd8c5bcfea271a
Ian VanSchooten <ian@badgelabsllc.com>
2015-09-28 00:59:55 -0400

    Add CLI
    Prompts user for username and password, and then gets and prints 100 users ...

--- index.js
+++ index.js
@@ -8,39 +8,30 @@ const api = require('./lib/api-client');
 const contacts = require('./lib/contacts');
 
 //------------------------------------------------------------------------------
-// Setup
-//------------------------------------------------------------------------------
-
-let options = {
-  method   : 'POST',
-  path     : '/api/v1/json',
-  username : 'jim@demo.nutshell.com',
-  password : '43c789d483fd76547b1f157e3cf5e580b95b9d8c'
-};
-
-//------------------------------------------------------------------------------
 // Public Interface
 //------------------------------------------------------------------------------
 
-let start = Date.now();
-api.newNutshell(options, function (err, nutshell) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  let state = { nutshell };
-  contacts.findFirstHundredEmails(state, function (err, contacts) {
+exports.getFirstHundredContactsWithEmails = function (options, cb) {
+  options.method = 'POST';
+  options.path = '/api/v1/json';
+
+  api.newNutshell(options, function (err, nutshell) {
     if (err) {
       console.error(err);
       return;
     }
-    // console.log(contacts.length);
-    if (contacts.length >= 100) {
-      let contactNames = contacts.map(function (contact) {
-        return contact.name.displayName;
-      });
-      console.log(contactNames);
-      console.log(`Retrieved ${contactNames.length} contacts with emails in ${(Date.now() - start) / 1000} seconds`);
-    }
+    let state = { nutshell };
+    contacts.findFirstHundredEmails(state, function (err, contacts) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      if (contacts.length >= 100) {
+        let contactNames = contacts.map(function (contact) {
+          return contact.name.displayName;
+        });
+        cb(err, contactNames);
+      }
+    });
   });
-});
+};
