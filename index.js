0fe76bc5190c9c21c78396da3c0557e08c2a5b7b
Ian VanSchooten <ian@badgelabsllc.com>
2015-09-30 00:15:18 -0400

    Expand options in CLI
    It is now possible to run various `findXxxx`-style calls against the Nutshell API.
    The user is presented a series of questions after running `nutshell`, such as
    the things they want to find, the number they want, any requirements on order, and
    properties on which to filter.
    
    Still to do:
    
    * Add tests :(
    * Clean up/improve output
    * Probably more

--- index.js
+++ index.js
@@ -10,22 +10,33 @@ const api = require('./lib/api-client');
 // Public Interface
 //------------------------------------------------------------------------------
 
-exports.getFirstHundredContactsWithEmails = function (options, cb) {
-  options.method = 'POST';
-  options.path = '/api/v1/json';
+exports.find = function (entity, args, cb) {
+  let options = {
+    method   : 'POST',
+    path     : '/api/v1/json',
+    username : args.username,
+    password : args.password
+  };
 
   api.newNutshell(options, function (err, nutshell) {
     if (err) {
       console.error(err);
       return;
     }
-    let args = {
-      entity  : 'Contacts',
-      qty     : 100,
-      filter  : 'email',
-      orderBy : 'displayName'
-    };
-    nutshell.find(args, function (err, results) {
+    let findArgs = { entity };
+    if (args.qty) {
+      findArgs.qty = args.qty;
+    }
+    if (args.orderBy) {
+      findArgs.orderBy = args.orderBy;
+    }
+    if (args.orderDirection) {
+      findArgs.orderDirection = args.orderDirection;
+    }
+    if (args.filter) {
+      findArgs.filter = args.filter;
+    }
+    nutshell.find(findArgs, function (err, results) {
       if (err) {
         console.error(err);
         return;
